fb0ee8ab81d0e54d7873ed6456c978b03aa712c6
Veronika Mitichashvili <nika.mitichashili@gmail.com>
2022-02-22 00:05:33 +0400

    got the design + routing on home page working

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.scss";
 
 function Home() {
-  return <div className="home">home</div>;
+  return (
+    <div className="home">
+      <div className="container">
+        <Link to="/quiz">
+          <button type="button" className="btn-main">
+            Start Quiz
+          </button>
+        </Link>
+      </div>
+      <div className="history-container">
+        <h3>Last game result:</h3>
+        <p>score | date</p>
+        <Link to="/history">
+          <button type="button" className="btn-secondary">
+            See Full History
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
